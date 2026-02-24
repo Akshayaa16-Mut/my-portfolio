72f644405b8b39416574d4523bd629d24831d0f1
Akshayaa16-Mut <akshayamuthu166@gmail.com>
2026-02-24 22:59:16 +0530

    updated hero section buttons with spacing

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,53 @@
+import Link from "next/dist/client/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <section 
+     id="hero"
+     className="flex items-center min-h-[calc(100vh-4rem)] pt-16 pb-20 justify-center px-4 sm:px-6 lg:px-8">
+    
+     <div className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row items-center gap-12 w-full">
+      <div className="flex-1 text-center md:text-left">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-gray-600 dark:text-violet-200 trackign-wide mb-4">
+          Hi,I'm{" "}
+         <span className="text-blue-600 dark:text-pink-400">
+          Becks
+         </span>
+       </h1>
+       <h2 className="text-xl sm:text-2xl md:text-3xl font-medium text-gray-600 dark:text-gray-300 mb-8">
+         Cloud Engineer designing scalable, secure a high-performance cloud solutions. 
+       </h2>
+       <p className="text-base sm:text-lg text-blue-500 dark:text-pink-400 mb-10 max-w-2xl mx-auto md:mx-0">
+        I am passionate about learning cloud computing and building secure,scalable system using modern cloud technologies.
+       </p>
+       <div className="flex gap-4 mt-6">
+       <div className="inline-block p-[2px] rounded-xl bg-gradient-to-r from-pink-500 via-fuchsia-500 to-purple-500">
+        <Link
+         href="/projects"
+        className="block px-6 py-3 rounded-xl bg-black text-white hover:bg-gray-900 transition duration-300">
+        view projects
+        </Link>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="inline-block p-[2px] rounded-xl bg-gradient-to-r from-pink-500 via-fuchsia-500 to-purple-500">
+        <Link
+        href="/contact"
+        className="block px-6 py-3 rounded-xl bg-black text-white hover:bg-gray-900 transition duration-300">
+          Contact Me
+        </Link>
         </div>
-      </main>
-    </div>
+       </div>
+      </div> 
+      <div className="flex-1 flex justify-center md:justify-end w-full">
+        <div className="relatvie w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96 rounded-full overflow-hidden shadow-2xl">
+          <div className="w-full h-full bg-pink-100 dark:bg-pink-900/30 flex items-center justify-center">
+          <span className="text-indigo-400 dark:text-violet-500 font-small">
+            Hero image
+          </span>
+          </div>
+        </div>
+      </div>
+     </div>
+    </section>
   );
-}
+  }
